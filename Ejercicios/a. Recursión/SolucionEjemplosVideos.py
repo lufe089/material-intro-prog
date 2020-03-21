c5eaa037d7bc622c97f70f8f9ed813a08440eeb0
lufe089 <lufe089@gmail.com>
2020-03-20 20:30:00 -0500

    MOD: se actualiza el archivo con la solución de los ejercicios

--- Ejercicios/a. Recursión/SolucionEjemplosVideos.py
+++ Ejercicios/a. Recursión/SolucionEjemplosVideos.py
@@ -118,13 +118,7 @@ def convertirBinario(num):
         convertirBinario(num // 2)
         print(modulo)
 
-""""Dados dos números enteros positivos m y n, tal que m > n, para encontrar su máximo común divisor (es decir, el mayor entero positivo que divide a ambos): 
-• Dividir m por n para obtener el resto r (0 ≤ r < n) 
-• Si r = 0, el MCD es n 
-• Si no, el máximo común divisor es MCD(n,r)
-"""
-def algoritmoEuclides(num1, num2):
-    if
+
 
 ### Funciones para probar las funcionalidades recursivas
 
