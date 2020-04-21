1cc8ff628a31e965695d7caaf5d4eb99cfa09ff5
leonardo.alfonso <leonardo.alfonso@iterrabyte.com>
2020-04-21 03:29:17 -0500

    Reto 11

---
+++ Ejercicios/d. Diccionarios/Reto 11-12. 21 a 22-04.py
@@ -0,0 +1,23 @@
+# Debido al aumento en la obesidad en los niños una empresa contra los dulces llamada SInoMelaSeMeLasInvento S.A. quiere poner una ley para que los niños no puedan salir a pedir dulces en Halloween.
+# Los defensores de los dulces desean demostrarle a la empresa SInoMelaSeMeLasInvento S.A. que los niños no comen tantos dulces como ellos creen y que son los padres los que terminan aprovechando la recolección; mientras que ellos controlan lo que le dan a sus hijos. Para ello, ud debe crear un programa que pregunte la información siguiente información a N familias:
+# Cantidad de hijos, cantidad de dulces recogidos, cantidad de dulces que cada niño comió, cantidad de dulces que se regalaron.
+# A partir de esa información, ud debe completar la lista con:
+# Porcentaje de dulces consumidos por niños, porcentaje de dulces consumidos por invitados y porcentaje de dulces consumidos por los padres.
+
+# Cuando termine de solicitar la información, ud tendrá una matriz con todos los datos necesarios para hacer un estudio y así no permitir que la empresa SInoMelaSeMeLasInvento S.A. prohiba Halloween. Para ello ud mostrará los siguientes datos:
+
+# Cantidad de familias con un porcentaje mayor de 50% de consumo de dulces por parte de los niños
+# Cantidad de familias con un porcentaje mayor de 50% de consumo de dulces por parte de los invitados
+# Cantidad de familias con un porcentaje mayor de 50% de consumo de dulces por parte de los padres
+# Cantidad de dulces totales
+# Cantidad de dulces comidos por niños en total
+# Cantidad de dulces comidos por invitados en total
+# Cantidad de dulces comidos por adultos en total
+# Promedio de dulces comidos por niños y por adultos en total en cada familia.
+# Porcentaje de dulces comidos por niños en total
+# Porcentaje de dulces comidos por adultos en total
+# Debe agregar todos los datos calculados a un diccionario.
+
+# Construya una función que reciba como parámetros (entradas) un diccionario, un variable correspondiente a la llave y una variable correspondiente al valor. Esta función debe agregar dentro del diccionario la nueva llave valor y devolver el diccionario modificado.
+# Construya una función que tome el diccionario e imprima la información del estudio
+# Recuerde mantener la responsabilidad de las funciones. (Una función solo debe realizar una acción específica.)
